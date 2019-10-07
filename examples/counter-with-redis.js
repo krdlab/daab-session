381b82e546252c3bafd6e9e537c38a562bd90784
krdlab <krdlab@gmail.com>
2019-10-08 00:17:30 +0900

    Make it possible to use 'connect-redis'

---
+++ examples/counter-with-redis.js
@@ -0,0 +1,27 @@
+// Copyright (c) 2019 Sho Kuroda <krdlab@gmail.com>
+// 
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+'use strict';
+
+const withSession = require('daab-session');
+const redis = require('redis');
+const connect = require('connect-redis');
+
+const client = redis.createClient();
+const RedisStore = connect(withSession);
+
+const options = {
+  store: new RedisStore({ client })
+};
+
+const actions = robot => {
+  robot.respond(/count$/i, res => {
+    // NOTE: increase a counter for each (talk, user)
+    let i = res.session.count || 0;
+    res.session.count = ++i;
+    res.send('' + i);
+  });
+};
+
+module.exports = withSession(options, actions);
--- index.js
+++ index.js
@@ -4,95 +4,75 @@
 // https://opensource.org/licenses/MIT
 'use strict';
 
-// interface Store {
-//     load();
-//     get(id: string): any;
-//     set(id: string, data: {id: string});
-//     save(session: {id: string})
-//     destroy(id: string);
-// }
+const Store = require('./lib/store.js');
+const MemoryStore = require('./lib/store/memory.js');
+const Session = require('./lib/session.js');
 
-class MemoryStore /* implements Store */ {
-    constructor() {
-        this.sessions = {};
-    }
-
-    load() { /* nop */ }
-
-    get(id) {
-        const data = this.sessions[id];
-        return data ? new Session(id, JSON.parse(data)) : null;
-    }
-    set(id, data) {
-        this.sessions[id] = JSON.stringify(data);
-    }
-    save(session) {
-        this.set(session.id, session);
-    }
-    destroy(id) {
-        delete this.sessions[id];
-    }
-}
-
-class Session {
-    constructor(id, data) {
-        this.id = id;
-        this._invalid = false;
-        if (!!data && typeof data === 'object') {
-            for (let prop in data) {
-                if (!(prop in this)) {
-                    this[prop] = data[prop];
-                }
-            }
+const middleware = ({ store, sessionable }) => {
+    const getSession = (res, cb) => {
+        if (!sessionable(res)) {
+            cb(null, null);
         }
-    }
-
-    get isInvalid() {
-        return this._invalid;
-    }
-
-    invalidate() {
-        this._invalid = true;
-    }
-}
-
-const middleware = ({store, sessionable}) => {
-    const getSession = res => (store.find(res) || store.generate(res));
+        store.find(res, (err, se) => {
+            if (!!se) {
+                cb(err, se);
+            } else {
+                cb(null, store.generate(res))
+            }
+        });
+    };
+    const endSession = session => {
+        if (!session) {
+            return;
+        }
+        if (session.isInvalid) {
+            session.destroy();
+        } else {
+            session.save();
+        }
+    };
 
     return (context, next , done) => {
         const res = context.response;
-        if (res.session) {
+        if (!!res.session) {
             next();
             return;
         }
 
-        res.session = sessionable(res) ? getSession(res) : null;
-        try {
-            next();
-        } finally {
-            if (res.session) {
-                if (res.session.isInvalid) {
-                    store.destroy(res.session.id)
-                } else {
-                    store.save(res.session);
-                }
+        getSession(res, (err, session) => {
+            // ! TODO: err
+            res.session = session;
+            try {
+                next();
+            } finally {
+                endSession(res.session);
             }
-        }
+        });
     };
 };
 
 const withSession = (options, actions) => {
     const sessionable = options.sessionable || (res => (res.message.room && res.message.user));
-    const createId = options.createId || (res => (`${res.message.room}-${res.message.user.id}`));
+    const sessionIdPrefix = options.sessionIdPrefix || 'daab.';
+    const createId = options.createId || (res => (`${sessionIdPrefix}${res.message.room}.${res.message.user.id}`));
     const store = options.store || new MemoryStore();
 
-    store.find = res => (store.get(createId(res)));
-    store.generate = res => (new Session(createId(res), null));
+    store.find = (res, cb) => {
+        res.sessionID = createId(res);
+        res.sessionStore = store;
+        store.get(res.sessionID, (err, data) => cb(err, new Session(res, data)));
+    };
+    store.generate = res => {
+        res.sessionID = createId(res);
+        res.sessionStore = store;
+        return new Session(res, {});
+    };
 
     return robot => {
-        robot.listenerMiddleware(middleware({store, sessionable}, createId));
+        robot.listenerMiddleware(middleware({ store, sessionable }));
         actions(robot);
     };
 }
 
 module.exports = withSession;
+module.exports.Store = Store; // * for connect-redis
