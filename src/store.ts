9bdcd74e1663c053255cae4bd630d0587a40cdf1
krdlab <krdlab@gmail.com>
2019-12-29 22:56:30 +0900

    refactor: Format code, pass 'not implemented' error, remove comment

--- src/store.ts
+++ src/store.ts
@@ -13,8 +13,12 @@ export const nop: StoreCallback<any, any> = (err, data) => {};
 
 // * NOTE: A stored data is an internal data itself held by a Session type value. It's not the Session type.
 export abstract class Store<R, D> extends EventEmitter {
-    public generate: (res: daab.Response<R, D>) => Session<R, D> = res => new Session(res, undefined);
-    public find: (res: daab.Response<R, D>, cb: StoreCallback<R, D>) => void = (_r, _c) => {};
+
+    public generate: (res: daab.Response<R, D>) => Session<R, D> =
+        res => new Session(res, undefined);
+
+    public find: (res: daab.Response<R, D>, cb: StoreCallback<R, D>) => void =
+        (_r, cb) => { cb(new Error('not implemented')); };
 
     constructor() {
         super();
@@ -39,19 +43,4 @@ export abstract class Store<R, D> extends EventEmitter {
     createSession(id: string, data: Partial<D>): Session<R, D> {
         return new Session({ sessionID: id, sessionStore: this }, data);
     }
-
-    /* FIXME
-    load(sid, fn) {
-        this.get(sid, (err, sess) => {
-            if (err) {
-                return fn(err);
-            }
-            if (!sess) {
-                return fn();
-            }
-            const req = { sessionID: sid, sessionStore: this };
-            return fn(null, this.createSession(req, sess));
-        });
-    }
-    */
 }
